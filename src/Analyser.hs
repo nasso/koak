ccdb07b888a78c62112f43ef7a1ff2238251a1b1
nasso <11479594+nasso@users.noreply.github.com>
2022-02-10 23:30:10 +0100

    Add project skeleton (#7)
    * Add basic AST type definitions
    
    * Update AST type definitions
    
    * Add project skeleton

---
+++ src/Analyser.hs
@@ -0,0 +1,16 @@
+module Analyser
+  ( AnalyserConfig,
+    analyseProgram,
+  )
+where
+
+import Syntax (Program, ProgramT)
+
+-- | Configuration for the analyser.
+data AnalyserConfig = AnalyserConfig
+  {
+  }
+  deriving (Show, Eq)
+
+analyseProgram :: AnalyserConfig -> Program -> Either String ProgramT
+analyseProgram = error "unimplemented analyseProgram"
---
+++ src/Compiler.hs
@@ -0,0 +1,22 @@
+module Compiler
+  ( CompilerConfig (..),
+    CompilationTarget (..),
+    compileProgram,
+  )
+where
+
+import Data.Word (Word8)
+import Syntax (ProgramT)
+
+-- | Configuration for the compiler.
+newtype CompilerConfig = CompilerConfig
+  { target :: CompilationTarget
+  }
+  deriving (Show, Eq)
+
+-- | A compilation target.
+data CompilationTarget = Assembly | Bitcode deriving (Show, Eq)
+
+-- | Compile a program.
+compileProgram :: CompilerConfig -> ProgramT -> [Word8]
+compileProgram = error "unimplemented compileProgram"
