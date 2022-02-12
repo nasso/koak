904f0e6c8a72380709062e487f39681d5bf16ab4
nasso <nassomails@gmail.com>
2022-02-12 18:21:42 +0100

    Add haddock comments for analyser types

--- src/Koa/Analyser.hs
+++ src/Koa/Analyser.hs
@@ -19,16 +19,19 @@ newtype AnalyserConfig = AnalyserConfig
   }
   deriving (Show, Eq)
 
+-- | A warning that can be emitted by the analyser.
 data AnalyserWarningType
   = WDeadCode
   deriving (Show, Eq)
 
+-- | An error indicating that the program is invalid.
 data AnalyserErrorType
   = EUndefinedSymbol Ident
   | ETypeMismatch {eExpected :: Type, eActual :: Type}
   | EWarn AnalyserWarningType
   deriving (Show, Eq)
 
+-- | A location in the source code.
 data AnalyserLocation
   = LDef Definition
   | LExpr Expr
@@ -36,10 +39,13 @@ data AnalyserLocation
   | LIdent Ident
   deriving (Show, Eq)
 
+-- | A warning at a specific location.
 type AnalyserWarning = (AnalyserWarningType, AnalyserLocation)
 
+-- | An error at a specific location.
 type AnalyserError = (AnalyserErrorType, AnalyserLocation)
 
+-- | The result of the analyser.
 type AnalyserResult =
   Either (NonEmpty AnalyserError) (ProgramT, [AnalyserWarning])
 
