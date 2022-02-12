2f094d3c717b5131e10a71be189eb117c12f5359
nasso <nassomails@gmail.com>
2022-02-12 20:15:33 +0100

    Add basic block type checking

--- src/Koa/Analyser.hs
+++ src/Koa/Analyser.hs
@@ -43,6 +43,7 @@ data AnalyserLocation
   | LExpr Expr
   | LStmt Stmt
   | LIdent Ident
+  | LBlock Block
   deriving (Show, Eq)
 
 -- | A warning at a specific location.
@@ -75,10 +76,65 @@ newtype Analyser a = Analyser
       MonadError AnalyserError
     )
 
+-- | Runs the static analyser on the given program AST.
 analyseProgram :: AnalyserConfig -> Program -> AnalyserResult
 analyseProgram cfg ast =
   runExcept $ runWriterT $ runReaderT (runAnalyser $ program ast) cfg
 
+-- | Analyse a program.
 program :: Program -> Analyser ProgramT
-program (Program []) = pure (Program [])
-program (Program (d : _)) = throwError (EUnimplemented, LDef d)
+program (Program defs) = Program <$> traverse definition defs
+
+-- | Analyse a definition.
+definition :: Definition -> Analyser DefinitionT
+definition (DFn name args rety body) =
+  DFn name args rety <$> checkBlock rety body
+
+-- | Analyse a block, verifying that it evaluates to the expected type.
+checkBlock :: Type -> Block -> Analyser BlockT
+checkBlock expectedTy b =
+  do
+    block' <- block b
+    let actualTy = blockType block'
+    when (actualTy /= expectedTy) $
+      throwError
+        ( ETypeMismatch {eExpected = expectedTy, eActual = actualTy},
+          LBlock b
+        )
+    pure block'
+
+-- | Analyse a block.
+block :: Block -> Analyser BlockT
+block (BExpr stmts mExpr) =
+  do
+    stmts' <- traverse statement stmts
+    expr' <- traverse expression mExpr
+    pure $ BExpr stmts' expr'
+
+-- | Analyse a statement.
+statement :: Stmt -> Analyser StmtT
+statement = error "not implemented"
+
+-- | Analyse an expression.
+expression :: Expr -> Analyser ExprT
+expression (Expr (ELit lit)) = pure $ ExprT (ELit lit, litType lit)
+expression _ = error "not implemented"
+
+-- | Get the type of a block.
+blockType :: BlockT -> Type
+blockType (BExpr _ Nothing) = TEmpty
+blockType (BExpr _ (Just e)) = exprType e
+
+-- | Get the type of an expression.
+exprType :: ExprT -> Type
+exprType (ExprT (_, t)) = t
+
+-- | Get the type of a literal.
+--
+-- TODO: the type of a literal can be ambiguous, e.g. @1@ can be an integer of
+-- any size. We could infer the type from the context in which it is used, or
+-- return a list of the possible types.
+litType :: Literal -> Type
+litType LEmpty = TEmpty
+litType (LInt _) = TInt32
+litType (LFloat _) = TFloat64
