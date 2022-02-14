26b051e7acb8de86334fc4e49cb66983ede9fe11
nasso <nassomails@gmail.com>
2022-02-14 02:36:03 +0100

    Reorder parsing functions

--- src/Koa/Parser.hs
+++ src/Koa/Parser.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE TypeFamilies #-}
+
 module Koa.Parser
   ( ParserConfig (..),
     parseProgram,
   )
 where
 
-import Koa.Syntax
 import Control.Monad.Parser
+import Koa.Syntax
 
 -- | Parser configuration.
 data ParserConfig = ParserConfig
@@ -14,16 +15,16 @@ data ParserConfig = ParserConfig
   }
   deriving (Show, Eq)
 
-lexeme :: CharParser p => p a -> p a
-lexeme p = spaces *> p <* spaces
-  where
-    spaces = many $ oneOf " \n\r\t"
+-- | Parse a program from a string.
+parseProgram :: ParserConfig -> String -> Either String Program
+parseProgram _ [] = Right $ Program []
+parseProgram _ a =
+  case runStringParser (program <* eof) a of
+    Parsed v _ _ -> Right v
+    _ -> Left "Parse error"
 
-letter :: CharParser p => p Char
-letter = oneOf "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"
-
-ident :: CharParser p => p Ident
-ident = lexeme $ Ident <$> many1 letter
+program :: CharParser p => p Program
+program = Program <$> many (lexeme definition)
 
 -- TODO: Parse the return type and the expression blocks
 definition :: CharParser p => p Definition
@@ -31,12 +32,13 @@ definition = do
   name <- ident
   return $ DFn name [] TEmpty (BExpr [] $ Expr $ ELit LEmpty)
 
-program :: CharParser p => p Program
-program = Program <$> many (lexeme definition)
+ident :: CharParser p => p Ident
+ident = lexeme $ Ident <$> many1 letter
 
--- | Parse a program from a string.
-parseProgram :: ParserConfig -> String -> Either String Program
-parseProgram _ [] = Right $ Program []
-parseProgram _ a = case runStringParser (program <* eof) a of
-  Parsed v _ _ -> Right v
-  _ -> Left "Parse error"
+letter :: CharParser p => p Char
+letter = oneOf "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"
+
+lexeme :: CharParser p => p a -> p a
+lexeme p = spaces *> p <* spaces
+  where
+    spaces = many $ oneOf " \n\r\t"
