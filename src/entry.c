48b0e3a046be02363b80b35829863596b4e59349
Skyf0l <tom.rorato@epitech.eu>
2022-02-28 11:44:13 +0100

    Add return main types to entry

--- src/entry.c
+++ src/entry.c
@@ -1,6 +1,18 @@
+#if defined(KOA_MAIN_RETURNS_I32)
 int __koa_main(void);
+#else /* if defined(KOA_MAIN_RETURNS_EMPTY) */
+void __koa_main(void);
+#endif
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    return __koa_main();
+    int ret;
+
+#if defined(KOA_MAIN_RETURNS_I32)
+    ret = __koa_main();
+#else /* if defined(KOA_MAIN_RETURNS_EMPTY) */
+    __koa_main();
+    ret = 0;
+#endif
+    return ret;
 }
