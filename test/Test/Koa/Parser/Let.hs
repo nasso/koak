86c2033725951d11bedf288907fe6e61453b9f79
nasso <nassomails@gmail.com>
2022-02-28 01:38:03 +0100

    Format `Let.hs`

--- test/Test/Koa/Parser/Let.hs
+++ test/Test/Koa/Parser/Let.hs
@@ -9,113 +9,113 @@ import Test.Tasty.HUnit
 valid :: [TestTree]
 valid =
   [ testCase "non mutable ident a" $
-    assertProgram
-    "fn f(): () { let a = 1; }"
-    ( Program
-      [ DFn
-        (Ident "f")
-        []
-        TEmpty
-        ( BExpr
-          [ SLet
-            (PIdent $ Ident "a")
-            Nothing
-            (litI32 1)
-          ]
-          Nothing
-        )
-      ]
-    ),
-  testCase "mutable ident a" $
-    assertProgram
-    "fn f(): () { let mut a = 1; }"
-    ( Program
-      [ DFn
-        (Ident "f")
-        []
-        TEmpty
-        ( BExpr
-          [ SLet
-            (PMutIdent $ Ident "a")
-            Nothing
-            (litI32 1)
-          ]
-          Nothing
-        )
-      ]
-    ),
-  testCase "non mutable wildcard" $
-    assertProgram
-    "fn f(): () { let _ = 1; }"
-    ( Program
-      [ DFn
-        (Ident "f")
-        []
-        TEmpty
-        ( BExpr
-          [ SLet
-            PWildcard
-            Nothing
-            (litI32 1)
-          ]
-          Nothing
-        )
-      ]
-    ),
+      assertProgram
+        "fn f(): () { let a = 1; }"
+        ( Program
+            [ DFn
+                (Ident "f")
+                []
+                TEmpty
+                ( BExpr
+                    [ SLet
+                        (PIdent $ Ident "a")
+                        Nothing
+                        (litI32 1)
+                    ]
+                    Nothing
+                )
+            ]
+        ),
+    testCase "mutable ident a" $
+      assertProgram
+        "fn f(): () { let mut a = 1; }"
+        ( Program
+            [ DFn
+                (Ident "f")
+                []
+                TEmpty
+                ( BExpr
+                    [ SLet
+                        (PMutIdent $ Ident "a")
+                        Nothing
+                        (litI32 1)
+                    ]
+                    Nothing
+                )
+            ]
+        ),
+    testCase "non mutable wildcard" $
+      assertProgram
+        "fn f(): () { let _ = 1; }"
+        ( Program
+            [ DFn
+                (Ident "f")
+                []
+                TEmpty
+                ( BExpr
+                    [ SLet
+                        PWildcard
+                        Nothing
+                        (litI32 1)
+                    ]
+                    Nothing
+                )
+            ]
+        ),
     testCase "typed non mutable ident a" $
-    assertProgram
-    "fn f(): () { let a: i32 = 1; }"
-    ( Program
-      [ DFn
-        (Ident "f")
-        []
-        TEmpty
-        ( BExpr
-          [ SLet
-            (PIdent $ Ident "a")
-            (Just TInt32)
-            (litI32 1)
-          ]
-          Nothing
+      assertProgram
+        "fn f(): () { let a: i32 = 1; }"
+        ( Program
+            [ DFn
+                (Ident "f")
+                []
+                TEmpty
+                ( BExpr
+                    [ SLet
+                        (PIdent $ Ident "a")
+                        (Just TInt32)
+                        (litI32 1)
+                    ]
+                    Nothing
+                )
+            ]
+        ),
+    testCase "typed mutable ident a" $
+      assertProgram
+        "fn f(): () { let mut a: i32 = 1; }"
+        ( Program
+            [ DFn
+                (Ident "f")
+                []
+                TEmpty
+                ( BExpr
+                    [ SLet
+                        (PMutIdent $ Ident "a")
+                        (Just TInt32)
+                        (litI32 1)
+                    ]
+                    Nothing
+                )
+            ]
+        ),
+    testCase "typed non mutable wildcard" $
+      assertProgram
+        "fn f(): () { let _: i32 = 1; }"
+        ( Program
+            [ DFn
+                (Ident "f")
+                []
+                TEmpty
+                ( BExpr
+                    [ SLet
+                        PWildcard
+                        (Just TInt32)
+                        (litI32 1)
+                    ]
+                    Nothing
+                )
+            ]
         )
-      ]
-    ),
-  testCase "typed mutable ident a" $
-    assertProgram
-    "fn f(): () { let mut a: i32 = 1; }"
-    ( Program
-      [ DFn
-        (Ident "f")
-        []
-        TEmpty
-        ( BExpr
-          [ SLet
-            (PMutIdent $ Ident "a")
-            (Just TInt32)
-            (litI32 1)
-          ]
-          Nothing
-        )
-      ]
-    ),
-  testCase "typed non mutable wildcard" $
-    assertProgram
-    "fn f(): () { let _: i32 = 1; }"
-    ( Program
-      [ DFn
-        (Ident "f")
-        []
-        TEmpty
-        ( BExpr
-          [ SLet
-            PWildcard
-            (Just TInt32)
-            (litI32 1)
-          ]
-          Nothing
-        )
-      ]
-    )
   ]
 
 letTests :: [TestTree]
