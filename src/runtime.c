1f46c2d8e12641f3b60f3e42125dd92c23bc7a87
Skyf0l <tom.rorato@epitech.eu>
2022-02-28 23:20:08 +0100

    Update runtime entry return value

--- src/runtime.c
+++ src/runtime.c
@@ -1,7 +1,9 @@
 #if defined(KOA_MAIN_RETURNS_I32)
 int __koa_main(void);
-#else /* if defined(KOA_MAIN_RETURNS_EMPTY) */
+#elif defined(KOA_MAIN_RETURNS_EMPTY)
 void __koa_main(void);
+#else
+    #error "Unknown return type for koa_main"
 #endif
 
 int main(void)
@@ -10,7 +12,7 @@ int main(void)
 
 #if defined(KOA_MAIN_RETURNS_I32)
     ret = __koa_main();
-#else /* if defined(KOA_MAIN_RETURNS_EMPTY) */
+#elif defined(KOA_MAIN_RETURNS_EMPTY)
     __koa_main();
     ret = 0;
 #endif
