3b821dd98aa0605096dcc2745e0b2a4a8439e812
nasso <11479594+nasso@users.noreply.github.com>
2022-03-01 14:25:47 +0100

    Update src/Koa/Linker.hs

--- src/Koa/Linker.hs
+++ src/Koa/Linker.hs
@@ -11,10 +11,11 @@ import System.IO.Temp
 import System.Process
 
 linkFilesToExecutable :: FilePath -> [FilePath] -> IO ()
-linkFilesToExecutable out objs = withSystemTempFile "main.c" $ \p h ->
-  hPutStr h entry
-    >> hFlush h
-    >> rawSystem "cc" ("-o" : out : p : objs)
-    >> pure ()
+linkFilesToExecutable out objs = 
+  withSystemTempFile "main.c" $ \p h ->
+    hPutStr h entry
+      >> hFlush h
+      >> rawSystem "cc" ("-o" : out : p : objs)
+      >> pure ()
   where
     entry = $(embedStringFile "src/runtime.c") :: String
