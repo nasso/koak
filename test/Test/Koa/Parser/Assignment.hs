c628123303fb803bb6aafd5a331080b659d44677
Alexandre Flion <dev@huntears.com>
2022-03-04 18:46:55 +0100

    Add tests for block assignments

---
+++ test/Test/Koa/Parser/Assignment.hs
@@ -0,0 +1,66 @@
+module Test.Koa.Parser.Assignment (assignmentTests) where
+
+import Koa.Syntax
+import Test.Koa.Parser.Utils
+import Test.Koa.Util
+import Test.Tasty
+import Test.Tasty.HUnit
+
+valid :: [TestTree]
+valid =
+  [ testCase "typed non mutable wildcard block empty" $
+      assertProgram
+        "fn f(): () { a = { }; }"
+        ( Program
+            [ DFn
+                (Ident "f")
+                []
+                TEmpty
+                ( BExpr
+                    [ SExpr $
+                        Expr
+                          ( EAssign
+                              (Ident "a")
+                              ( Expr $
+                                  EBlock $
+                                    BExpr [] Nothing
+                              )
+                          )
+                    ]
+                    Nothing
+                )
+            ]
+        ),
+    testCase "typed non mutable wildcard block" $
+      assertProgram
+        "fn f(): () { a = { 5 }; }"
+        ( Program
+            [ DFn
+                (Ident "f")
+                []
+                TEmpty
+                ( BExpr
+                    [ SExpr $
+                        Expr
+                          ( EAssign
+                              (Ident "a")
+                              ( Expr $
+                                  EBlock $
+                                    BExpr [] (Just $ litI32 5)
+                              )
+                          )
+                    ]
+                    Nothing
+                )
+            ]
+        )
+  ]
+
+invalid :: [TestTree]
+invalid = []
+
+assignmentTests :: [TestTree]
+assignmentTests =
+  [ testGroup "Valid" valid,
+    testGroup "Invalid" invalid
+  ]
